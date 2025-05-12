36e759c37fa2f9c1e5d106f92be82a34b331e1ec
Vatsal Parmar <vatsalparmar308@gmail.com>
2025-05-12 23:58:45 +0530

    hide creds

--- e-commerce-backend/connect.js
+++ e-commerce-backend/connect.js
@@ -1,6 +1,7 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://vatsalparmar1998:vatsal2024@cluster0.vdeuf0q.mongodb.net/";
+require('dotenv').config(); 
+const uri = process.env.MONGO_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
